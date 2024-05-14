9ed6e8062e9c2830042fc30c017b40dab3f499ba
LouCapener <louise.capener@manchester.ac.uk>
2024-05-14 13:54:23 +0100

    ..

---
+++ install.r
@@ -0,0 +1,14 @@
+# Install leaflet for interactive map visualizations
+install.packages("leaflet", dependencies = TRUE)
+
+# Install plotly for interactive plotting
+install.packages("plotly", dependencies = TRUE)
+
+# Install sf for handling geographic data
+install.packages("sf", dependencies = TRUE)
+
+# Install ggplot2 for data visualization (if not already handled by conda)
+install.packages("ggplot2", dependencies = TRUE)
+
+# Install ggplot2 for data visualization (if not already handled by conda)
+install.packages("stringr", dependencies = TRUE)
