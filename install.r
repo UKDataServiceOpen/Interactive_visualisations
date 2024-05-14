2dd4bc4488f59ffd013eee53b758c7bea6120d46
LouCapener <louise.capener@manchester.ac.uk>
2024-05-14 16:13:08 +0100

    Hmm.

--- install.r
+++ install.r
@@ -2,9 +2,14 @@ packages <- c("plotly", "ggplot2", "dplyr", "leaflet", "sf", "readr")
 
 for (pkg in packages) {
   if (!require(pkg, character.only = TRUE)) {
+    print(paste("Installing", pkg))
     install.packages(pkg, dependencies = TRUE)
     if (!require(pkg, character.only = TRUE)) {
-      cat(sprintf("Failed to install %s.\n", pkg))
+      cat(sprintf("FAILED TO INSTALL %s.\n", pkg))
+    } else {
+      print(paste(pkg, "INSTALLED SUCCESFFULLYY"))
     }
+  } else {
+    print(paste(pkg, "ALREADY INSTALLED"))
   }
 }
