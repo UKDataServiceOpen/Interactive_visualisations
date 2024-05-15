30d648b70c297e503678316d02d66162a9b70cbb
LouCapener <louise.capener@manchester.ac.uk>
2024-05-15 15:31:47 +0100

    .

--- install.r
+++ install.r
@@ -1,5 +1,10 @@
+# Set a CRAN mirror
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+# List of packages to install
 packages <- c("plotly", "ggplot2", "dplyr", "leaflet", "sf", "readr")
 
+# Install the packages
 for (pkg in packages) {
   if (!require(pkg, character.only = TRUE)) {
     print(paste("Installing", pkg))
