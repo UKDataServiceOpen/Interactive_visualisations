90bdb8338d82762887f4a217ad8139ad05d24cb7
LouCapener <louise.capener@manchester.ac.uk>
2024-05-22 20:01:38 +0100

    Not convinced this will work. Added some extra stuff to install.R

--- install.R
+++ install.R
@@ -1,2 +1,4 @@
 install.packages(c("readr", "sf", "leaflet", "dplyr","stringr", "ggplot2", "geojsonio", "plotly", "IRkernel"))
-IRkernel::
+devtools::install_github('IRkernel/IRkernel')
+IRkernel::installspec()
+IRkernel::installspec(name = 'ir33', displayname = 'R 3.3')
